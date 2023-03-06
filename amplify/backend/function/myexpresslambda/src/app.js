00bd4e0595d8f90f33544454814f5b37b5fed2b7
Blythe Waltman <blwa4890@colorado.edu>
2023-03-06 13:04:41 -0700

    update async func

--- amplify/backend/function/myexpresslambda/src/app.js
+++ amplify/backend/function/myexpresslambda/src/app.js
@@ -31,30 +31,30 @@ const {
   GetSecretValueCommand,
 } = require("@aws-sdk/client-secrets-manager");
 
-const secret_name = "amplify-test-env-key";
-
-const client = new SecretsManagerClient({
-  region: "us-east-1",
-});
-
-let response;
-
-try {
-  response = await client.send(
-    new GetSecretValueCommand({
-      SecretId: secret_name,
-      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-    })
-  );
-} catch (error) {
-  // For a list of exceptions thrown, see
-  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-  throw error;
-}
-
-const secret = response.SecretString;
+const getSecret = async () => {
+  const secret_name = "amplify-test-env-key";
+  const client = new SecretsManagerClient({
+    region: "us-east-1",
+  });
+  let response;
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    );
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+  }
+  const secret = response.SecretString;
+  return secret;
+};
 
 app.get("/items", function (req, res) {
+  const secret = getSecret();
   res.json({ success: "get call succeed!", secret });
 });
 
